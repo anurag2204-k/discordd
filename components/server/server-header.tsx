c2765c225e6719132092afe2912ecc9c1b08dc65
Anurag Khobragae <anuragk2204@gmail.com>
2025-01-01 22:48:37 +0530

    edit server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -53,6 +53,7 @@ export function ServerHeader({ server, role }: ServerHeaderProps) {
         )}
         {isAdmin && (
           <DropdownMenuItem
+            onClick = {()=>onOpen("editServer",{server})}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Server Settings
